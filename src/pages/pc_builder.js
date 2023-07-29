089fd95e9f394b3c0b85fbad23d830fff94fc90a
sifat18 <ibnsifat@gmail.com>
2023-07-29 16:14:29 +0600

    update routing

--- src/pages/pc_builder.js
+++ src/pages/pc_builder.js
@@ -6,52 +6,52 @@ import { CgSmartphoneRam } from 'react-icons/cg';
 import { ImPowerCord } from 'react-icons/im';
 import { GrStorage } from 'react-icons/gr';
 import { FiMonitor } from 'react-icons/fi';
-const categories=[
-    {
-      id:1,
-    label:"Motherboard",
-    name:"Motherboard",
-    icons:<BsMotherboard style={{fontSize:"4em"}}/>,
-},
-    {
-      id:2,
-    label:"RAM",
-    name:"RAM",
-    icons:<CgSmartphoneRam style={{fontSize:"4em"}}/>,
-},
-    {
-      id:3,
-    label:"CPU / Processor",
-    name:"CPU",
-    icons:<GoCpu style={{fontSize:"4em"}}/>,
-},
-    {
-      id:4,
-    label:"PSU",
-    name:"Power Supply Unit",
-    icons:<ImPowerCord style={{fontSize:"4em"}}/>,
-},
-    {
-      id:5,
-    label:"Storage Device",
-    name:"Storage Device",
-    icons:<GrStorage style={{fontSize:"4em"}}/>,
-},
-    {
-      id:6,
-    label:"Monitor",
-    name:"Monitor",
-    icons:<FiMonitor style={{fontSize:"4em"}}/>,
-},
+import Link from 'next/link'; //
 
-
-]
+const categories = [
+  {
+    id: 1,
+    label: 'Motherboard',
+    name: 'Motherboard',
+    icons: <BsMotherboard style={{ fontSize: '4em' }} />,
+  },
+  {
+    id: 2,
+    label: 'RAM',
+    name: 'RAM',
+    icons: <CgSmartphoneRam style={{ fontSize: '4em' }} />,
+  },
+  {
+    id: 3,
+    label: 'CPU / Processor',
+    name: 'CPU',
+    icons: <GoCpu style={{ fontSize: '4em' }} />,
+  },
+  {
+    id: 4,
+    label: 'PSU',
+    name: 'Power Supply Unit',
+    icons: <ImPowerCord style={{ fontSize: '4em' }} />,
+  },
+  {
+    id: 5,
+    label: 'Storage Device',
+    name: 'Storage Device',
+    icons: <GrStorage style={{ fontSize: '4em' }} />,
+  },
+  {
+    id: 6,
+    label: 'Monitor',
+    name: 'Monitor',
+    icons: <FiMonitor style={{ fontSize: '4em' }} />,
+  },
+];
 
 const StackOfCards = () => {
   return (
-    <div style={{margin:"2em 5em"}}>
-        <h2 style={{textAlign:"center"}}>Build PC By Yourself</h2>
-        <div style={{margin:"1em auto",height:"5px",fontWeight:'700', background: "#000",width:'15rem'}}></div>
+    <div style={{ margin: '2em 5em' }}>
+      <h2 style={{ textAlign: 'center' }}>Build PC By Yourself</h2>
+      <div style={{ margin: '1em auto', height: '5px', fontWeight: '700', background: '#000', width: '15rem' }}></div>
       <Row gutter={[16, 16]}>
         {categories?.map((item) => (
           <Col key={item.id} span={24}>
@@ -59,17 +59,20 @@ const StackOfCards = () => {
               <Row align="middle" gutter={[16, 16]}>
                 {/* Left Part */}
                 <Col span={6}>
-                {item?.icons}
-                  {/* <img src={item.image} alt="Product" style={{ width: '100%' }} /> */}
+                  {item?.icons}
+                  <h3 style={{ margin: '0' }}>{item.name}</h3>
                 </Col>
                 {/* Middle Part */}
                 <Col span={12}>
-                  <h3>{item.name}</h3>
-                  <p>{item.details}</p>
+                  {/* No details property here */}
+                  <p>{item.label}</p>
                 </Col>
                 {/* Right Part */}
                 <Col span={6} style={{ textAlign: 'right' }}>
-                  <Button type="primary">Select Component</Button>
+                  {/* Link should wrap the button text */}
+                  <Link href={{ pathname: `/category/${item?.name}`, query: { build: true } }}>
+                    Select Component
+                  </Link>
                 </Col>
               </Row>
             </Card>
@@ -83,6 +86,5 @@ const StackOfCards = () => {
 export default StackOfCards;
 
 StackOfCards.getLayout = function getLayout(page) {
-    return <RootLayout>{page}</RootLayout>;
-  };
-  
+  return <RootLayout>{page}</RootLayout>;
+};
